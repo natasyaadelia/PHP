f010c1136bcd8303e2dee5679f863b5d97f126fa
natasyaadelia <53499671+natasyaadelia@users.noreply.github.com>
2020-03-03 19:13:38 +0700

    Add files via upload

---
+++ dengan echo.php
@@ -0,0 +1,15 @@
+<?php
+    $nama="Natasya";
+    $sekolah="SMK Telkom Malang";
+    $umur=16;
+
+    //contoh 1
+    echo "Hallo namaku $nama usiaku $umur tahun";
+    echo "<br>";
+    echo "Aku sekolah di $sekolah";
+
+    //contoh 2
+    echo 'Halo namaku'.$nama.'usiaku'.$umur.'tahun';
+    echo "<br>";
+    echo 'Aku sekolah di'.$sekolah;
+?>
---
+++ latian 1.php
@@ -0,0 +1,5 @@
+<?php
+    echo "Namaku Natasya";
+    echo "<br>";
+    echo "Aku sedang belajar PHP di SMK Telkom Malang";
+?>
